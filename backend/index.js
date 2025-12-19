649775efe2840297a77dd4a4fdde360699cbdd38
SaiVardhan2007 <polampallisaivardhan2007@gmail.com>
2025-12-19 12:23:28 +0530

    solved error for admin folder deploy

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,8 @@ const allowedOrigins = [
   "http://localhost:3005",
   "http://localhost:5173",
   "http://localhost:4000",
-  process.env.FRONTEND_URL
+  process.env.FRONTEND_URL || "http://localhost:3000",
+  process.env.ADMIN_URL || "http://localhost:3001"
 ];
 
 app.use(cors({
